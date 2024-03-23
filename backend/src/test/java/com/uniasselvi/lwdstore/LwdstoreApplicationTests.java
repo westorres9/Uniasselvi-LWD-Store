487f5e7c9bd9717b55700a7e1ca149315c17755c
westorres9 <wester.torres9@gmail.com>
2024-03-23 14:52:41 -0300

    [BACKEND] - projeto backend criado

---
+++ backend/src/test/java/com/uniasselvi/lwdstore/LwdstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniasselvi.lwdstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LwdstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
