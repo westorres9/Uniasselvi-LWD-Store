1810ab0feeac9f1af0a00a1a9534b9d89db4d99c
Wellington <rodrigueswellington3@gmail.com>
2024-03-23 16:29:24 -0300

    [BACKEND]-criada as classes de Produto, categoria e marca

---
+++ backend/src/main/java/com/uniasselvi/lwdstore/entities/Brand.java
@@ -0,0 +1,54 @@
+package com.uniasselvi.lwdstore.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Brand {
+
+    private Long id;
+    private String name;
+    private String imageUrl;
+
+    private List<Product> products = new ArrayList<>();
+
+    public Brand() {
+    }
+
+    public Brand(Long id, String name, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
