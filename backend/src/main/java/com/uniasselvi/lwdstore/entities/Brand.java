ccb2488802571b877ab6a1f0a122212847749be8
westorres9 <wester.torres9@gmail.com>
2024-03-29 18:29:50 -0300

    [BACKEND] - create initial database seed

--- backend/src/main/java/com/uniasselvi/lwdstore/entities/Brand.java
+++ backend/src/main/java/com/uniasselvi/lwdstore/entities/Brand.java
@@ -1,11 +1,14 @@
 package com.uniasselvi.lwdstore.entities;
 
-import jakarta.persistence.*;
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_brand")
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_role(authority) VALUES ('ROLE_CLIENT');
+INSERT INTO tb_role(authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role(authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user(first_name, last_name, email, password, birth_date, phone_number) VALUES('Wester', 'Torres', 'wester@gmail.com', '123456', '1989-02-06', '34984456597');
+INSERT INTO tb_user(first_name, last_name, email, password, birth_date, phone_number) VALUES('Dayane', 'Torres', 'dayane@gmail.com', '123456', '1988-11-02', '34998738659');
+INSERT INTO tb_user(first_name, last_name, email, password, birth_date, phone_number) VALUES('Wellington', 'Santos', 'wellington@gmail.com', '123456', '1988-01-31', '34991208122');
