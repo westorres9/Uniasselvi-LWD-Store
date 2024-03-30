48e9d42af90420bd1295212173f7b4164b52782d
westorres9 <wester.torres9@gmail.com>
2024-03-30 10:02:43 -0300

    [BACKEND] - add camada de servico BrandService, metodo findById

--- backend/src/main/java/com/uniasselvi/lwdstore/services/BrandService.java
+++ backend/src/main/java/com/uniasselvi/lwdstore/services/BrandService.java
@@ -3,11 +3,14 @@ package com.uniasselvi.lwdstore.services;
 import com.uniasselvi.lwdstore.dto.BrandDTO;
 import com.uniasselvi.lwdstore.entities.Brand;
 import com.uniasselvi.lwdstore.repositories.BrandRepository;
+import com.uniasselvi.lwdstore.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class BrandService {
@@ -19,5 +22,12 @@ public class BrandService {
     public Page<BrandDTO> findAllPaged(Pageable pageable) {
         Page<Brand> brands = brandRepository.findAll(pageable);
         return brands.map(brand -> new BrandDTO(brand));
+    }
+
+    @Transactional(readOnly = true)
+    public BrandDTO findById(Long id) {
+       Optional<Brand> opt =  brandRepository.findById(id);
+       Brand brand = opt.orElseThrow(() -> new ResourceNotFoundException("Marca não encontrada"));
+       return new BrandDTO(brand);
     }
 }
