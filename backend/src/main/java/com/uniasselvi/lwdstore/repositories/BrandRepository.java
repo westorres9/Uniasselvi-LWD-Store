5206d9b0a938bb9612169e716e3aff2a352b449e
westorres9 <wester.torres9@gmail.com>
2024-03-30 08:57:52 -0300

    [BACKEND] - add camada de acesso a Dados BrandRepository

---
+++ backend/src/main/java/com/uniasselvi/lwdstore/repositories/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.uniasselvi.lwdstore.repositories;
+
+import com.uniasselvi.lwdstore.entities.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+}
