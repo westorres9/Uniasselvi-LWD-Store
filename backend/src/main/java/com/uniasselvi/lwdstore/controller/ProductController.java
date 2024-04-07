16fc01b9ac2ca0243a1f8b8a209ec5f23dba9450
westorres9 <wester.torres9@gmail.com>
2024-04-07 04:58:49 -0700

    [BACKEND] - add ProductController - findById

--- backend/src/main/java/com/uniasselvi/lwdstore/controller/ProductController.java
+++ backend/src/main/java/com/uniasselvi/lwdstore/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class ProductController {
     public ResponseEntity<Page<ProductDTO>> findAllPaged(Pageable pageable) {
         Page<ProductDTO> page = productService.findAllPaged(pageable);
         return ResponseEntity.ok().body(page);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = productService.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
