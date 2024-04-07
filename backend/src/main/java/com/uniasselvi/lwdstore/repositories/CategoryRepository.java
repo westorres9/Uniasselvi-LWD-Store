6b37dc7d7a10a3b4b17d6f33162eebf1b769cf92
westorres9 <wester.torres9@gmail.com>
2024-04-07 06:11:55 -0700

    [BACKEND] - Implementado busca paginada por nome de Marcas e Categorias

--- backend/src/main/java/com/uniasselvi/lwdstore/repositories/CategoryRepository.java
+++ backend/src/main/java/com/uniasselvi/lwdstore/repositories/CategoryRepository.java
@@ -1,9 +1,16 @@
 package com.uniasselvi.lwdstore.repositories;
 
 import com.uniasselvi.lwdstore.entities.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query("SELECT obj FROM Category obj " +
+            "WHERE UPPER(obj.name) LIKE LOWER(CONCAT('%', :name ,'%'))")
+    Page<Category> searchCategoriesPagedByName(String name, Pageable pageable);
 }
