ce9f47e993655010ef6cbfab4a8ed763f4f59a48
westorres9 <wester.torres9@gmail.com>
2024-04-07 04:56:12 -0700

    [BACKEND] - add ProductService - findById

--- backend/src/main/java/com/uniasselvi/lwdstore/services/ProductService.java
+++ backend/src/main/java/com/uniasselvi/lwdstore/services/ProductService.java
@@ -3,11 +3,14 @@ package com.uniasselvi.lwdstore.services;
 import com.uniasselvi.lwdstore.dto.ProductDTO;
 import com.uniasselvi.lwdstore.entities.Product;
 import com.uniasselvi.lwdstore.repositories.ProductRepository;
+import com.uniasselvi.lwdstore.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -19,5 +22,12 @@ public class ProductService {
     public Page<ProductDTO> findAllPaged(Pageable pageable) {
         Page<Product> page = productRepository.findAll(pageable);
         return page.map(product -> new ProductDTO(product));
+    }
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Optional<Product> optional = productRepository.findById(id);
+        Product product = optional.orElseThrow(() -> new ResourceNotFoundException("Produto não encontrado"));
+        return new ProductDTO(product);
     }
 }
