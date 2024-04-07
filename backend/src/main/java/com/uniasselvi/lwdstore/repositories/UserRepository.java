37f6f1dc9652e1483596cfb4d0dc7e5bb7791876
westorres9 <wester.torres9@gmail.com>
2024-04-07 06:16:53 -0700

    [BACKEND] - Implementado busca paginada por nome de Produtos e Usuários

--- backend/src/main/java/com/uniasselvi/lwdstore/repositories/UserRepository.java
+++ backend/src/main/java/com/uniasselvi/lwdstore/repositories/UserRepository.java
@@ -1,9 +1,17 @@
 package com.uniasselvi.lwdstore.repositories;
 
 import com.uniasselvi.lwdstore.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT obj FROM User obj " +
+            "WHERE LOWER(obj.firstName) LIKE LOWER(CONCAT('%', :name ,'%') " +
+            "OR LOWER(obj.lastName) LIKE LOWER(CONCAT('%', :name, '%')))")
+    Page<User> searchUserPagedByNameOrLastName(String name, Pageable pageable);
 }
