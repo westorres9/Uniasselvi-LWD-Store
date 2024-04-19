c80aa4ff9095a2be15e7ac79f7f369664abea872
westorres9 <wester.torres9@gmail.com>
2024-04-19 07:32:18 -0300

    [BACKEND] - add BCryptPasswordEncoder

--- backend/src/main/java/com/uniasselvi/lwdstore/config/SecurityConfig.java
+++ backend/src/main/java/com/uniasselvi/lwdstore/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.uniasselvi.lwdstore.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,5 +15,10 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable());
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
