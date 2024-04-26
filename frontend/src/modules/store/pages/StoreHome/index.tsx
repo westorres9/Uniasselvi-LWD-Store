6519c032ab380840a5e2a55c7d65c89b2675234c
Daytorres26 <dayanerosasantos26@gmail.com>
2024-04-25 21:45:05 -0300

    [FRONTEND] - pagina home card icons

--- frontend/src/modules/store/pages/StoreHome/index.tsx
+++ frontend/src/modules/store/pages/StoreHome/index.tsx
@@ -1,9 +1,11 @@
+import CardIconList from "../../components/CardIconList";
 import MainCarousel from "../../components/MainCarousel";
 
 export default function StoreHome() {
     return (
         <div>
             <MainCarousel/>
+           <CardIconList/>
         </div>
     )
 }
